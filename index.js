6424c137346d84d2e2720ba27fd314d6eb2908da
MK1499 <m.khaled1499@gmail.com>
2020-09-05 02:20:50 +0200

    solve error

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 app.post("/upload", upload.single("new-image"), (req, res) => {
   console.log("REQ : ", req.file.path);
-  res.send({ path: req.file.path ,, fullPath: __dirname + "/" + req.file.path});
+  res.send({ path: req.file.path , fullPath: __dirname + "/" + req.file.path});
 });
 
 let port = process.env.PORT || 3005;
